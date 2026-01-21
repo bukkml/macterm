7cd84807c979ab5d0e2d68f57400b8d6cb3012a1
bukk <mak.kurlovich@gmail.com>
2026-01-22 05:04:31 +0700

    Add new colorschemes and status lines for them

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,33 @@
+-- PURPLE COLORSCHEME
+-- return {
+--   "bukkml/lillilac.nvim",
+--   config = function()
+--     vim.cmd.colorscheme("lillilac")
+--   end,
+-- }
+
+
+--[[
 return {
-  "bukkml/lillilac.nvim",
-  config = function()
-    vim.cmd.colorscheme("lillilac")
+-- Baes2Tone_MallDark, Base2Tone_PorchDark
+    "atelierbram/Base2Tone-vim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme("Base2Tone_PorchDark")
+        vim.api.nvim_set_hl(0, "CursorLineNr", {fg = "#f2e3f7"})
+    end,
+}
+--]]
+
+
+-- PAPER COLORSCHEME
+return {
+  "thesimonho/kanagawa-paper.nvim",
+  lazy = false,
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme("kanagawa-paper-ink")
   end,
+  opts = {},
 }
--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -1,3 +1,31 @@
+-- Kunagawa colorscheme
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+  config = function()
+    local kanagawa_paper = require("lualine.themes.kanagawa-paper-ink")
+    require('lualine').setup({
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'searchcount', 'selectioncount', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      options = {
+        separator = nil,
+        section_separators = '',
+        component_separators = '',
+        theme = kanagawa_paper,
+      },
+    })
+  end,
+}
+
+-- New colorscheme
+--[[
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -60,7 +88,9 @@ return {
     })
   end,
 }
+--]]
 
+-- Old colorscheme
 --[[
 return {
 	--status line (the bar at the bottom)

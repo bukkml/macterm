514d69607bfd6a6137d9ec3385baddc7bb6ccc18
bukk <mak.kurlovich@gmail.com>
2026-02-14 02:31:06 +0700

    more useless files

--- nvim/shipwright_build.lua
+++
@@ -1,6 +0,0 @@
-local lushwright = require("shipwright.transform.lush")
-run(
-    require("lua/lush_theme/lillilac"),
-    lushwright.to_lua,
-    { patchwrite, "colors/lillilac.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
-)
